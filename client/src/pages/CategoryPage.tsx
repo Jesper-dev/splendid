89b9e9c24b6dd5baeca681a4a3a9d7ba2d0faa6e
JesperKYH <jesper.pettersson@student.kyh.se>
2021-09-28 13:36:41 +0200

    Category page working

--- client/src/pages/CategoryPage.tsx
+++ client/src/pages/CategoryPage.tsx
@@ -1,18 +1,79 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { CheckSlug } from "../api/checkSlug";
+import { AdCard } from "../components/discover/AdCard";
+import axios from "axios";
+
+interface DbObject {
+  _id: string;
+  title: string;
+  price: string[];
+  desc: string;
+  place: string;
+  name: string;
+  date: string;
+  category: string;
+  pic: string;
+}
 
 const CategoryPage = () => {
+  const [state, setState] = useState<{
+    dbObj: Array<DbObject>;
+    done: boolean;
+  }>({
+    dbObj: [
+      {
+        _id: "",
+        title: "",
+        price: [""],
+        desc: "",
+        place: "",
+        name: "",
+        date: "",
+        category: "",
+        pic: "",
+      },
+    ],
+    done: false,
+  });
   let slug = CheckSlug();
   const history = useHistory();
   useEffect(() => {
-    console.log(slug);
+    fetchDB();
   }, [slug]);
+
+  const fetchDB = () => {
+    //http://localhost:5000/api/ads/get
+    //https://splendidsrv.herokuapp.com/api/ads/get
+    axios
+      .post("https://splendidsrv.herokuapp.com/api/ads/get")
+      .then((res) => {
+        const newArr = res.data.filter(
+          (item: DbObject) => item.category === slug
+        );
+        setState((prev) => ({ ...prev, dbObj: newArr, done: true }));
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <section className="categoryPageContainer">
       <i className="fas fa-chevron-left" onClick={() => history.goBack()}></i>
-      <h1>{slug}</h1>
+      {state.done ? (
+        state.dbObj.map((item, i) => {
+          return (
+            <AdCard
+              key={i}
+              title={item.title}
+              price={item.price[0]}
+              place={item.place}
+              pic={item.pic ? item.pic : ""}
+            />
+          );
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
     </section>
   );
 };
